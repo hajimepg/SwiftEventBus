558b1d81d6a6d6253cdd79ab3a5e5193ceee1aca
Kenichi Saito <hajime@studiohff.net>
2016-09-11 20:47:10 +0900

    Support OS X

---
+++ SwiftEventBus-OSX/SwiftEventBus-OSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEventBus-OSX.h
+//  SwiftEventBus-OSX
+//
+//  Created by hajime on 2016/09/11.
+//  Copyright © 2016年 cesarferreira. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftEventBus-OSX.
+FOUNDATION_EXPORT double SwiftEventBus_OSXVersionNumber;
+
+//! Project version string for SwiftEventBus-OSX.
+FOUNDATION_EXPORT const unsigned char SwiftEventBus_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEventBus_OSX/PublicHeader.h>
+
+
